aef34b6863c8ee2403b82c32408fb964fcace624
RyanJenkins <rjenkins@mail.sfsu.edu>
2014-04-08 22:54:20 -0700

    Hecks yez, TLS for SMTP lives.

--- src/clojure/neveragain/tls.clj
+++ src/clojure/neveragain/tls.clj
@@ -6,24 +6,25 @@
     (less.awful [ssl :as las]))
   (:import (javax.net.ssl SSLSocketFactory)))
 
-(def verb-handler-map {
-	"STARTTLS" (fn [msg conn envl]
-		(let [sock (.createSocket (.getSocketFactory (las/ssl-context
-                                     (:key settings/keyfiles)
-                                     (:cert settings/keyfiles)
-                                     (:ca settings/keyfiles)))
-                   (:socket conn)
-                   (.getHostAddress (.getInetAddress (:socket conn)))
-                   (.getPort (:socket conn))
-                   true)]
-      (.setEnabledProtocols sock las/enabled-protocols)
-      (make-conn sock)))
-  })
+(defn handle-starttls [msg conn envl]
+  (write-out (:out conn) "220 Ready to start TLS")
+  (let [sock (.createSocket (.getSocketFactory (las/ssl-context
+                                   (:key settings/keyfiles)
+                                   (:cert settings/keyfiles)
+                                   (:ca settings/keyfiles)))
+                 (:socket conn)
+                 (.getHostName (.getRemoteSocketAddress (:socket conn)))
+                 (.getPort (:socket conn))
+                 true)]
+    (.setUseClientMode sock false)
+    (.setEnabledProtocols sock las/enabled-protocols)
+    (.startHandshake sock)
+    (make-conn sock)))
 
 
 (def extension-description {
 	:name "STARTTLS"
 	:advertise true
 	:install-handlers (fn [v-map]
-		(merge v-map verb-handler-map))
+		(merge v-map {"STARTTLS" handle-starttls}))
 	})
