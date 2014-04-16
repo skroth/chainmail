f3667600264f84a7d522123e23844524cc46aa27
RyanJenkins <rjenkins@mail.sfsu.edu>
2014-04-15 19:14:17 -0700

    Added `smtp` and `imap` lein aliases to start up each server.

--- project.clj
+++ project.clj
@@ -17,9 +17,13 @@
     [selmer "0.5.7"]
     [less-awful-ssl "0.1.1"]]
   :plugins [[lein-ring "0.7.1"]]
+  :profiles {:smtp {:main ^:skip-aot neveragain.core}
+             :imap {:main ^:skip-aot neveragain.imap}
+             :uberjar {:aot :all}}
+  :aliases {"smtp" ["with-profile" "smtp" "run"]
+            "imap" ["with-profile" "imap" "run"]}
   :main ^:skip-aot neveragain.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :ring {:handler webmail.routes/app})
