3f7e527ec14ea9f612c59d21c1e7165f44cf188c
Ryan Jenkins <rjenkins@mail.sfsu.edu>
2014-05-26 20:52:04 -0700

    Hahah, bow before my might powers of BASH!!!

--- rebuild_db.sh
+++ rebuild_db.sh
@@ -1,3 +1,18 @@
-rm neveragain.db
-sqlite3 neveragain.db < src/clojure/neveragain/schema.sql
-sqlite3 neveragain.db < test/neveragain/testdata.sql
+#!/usr/bin/env bash
+DB="neveragain.db"
+
+if [[ "$1" != "" ]]; 
+  then DB="$1"
+fi
+
+echo "rebuilding $DB ..."
+
+if [ -e "$DB" ]; then
+  echo "deleting old $DB"
+  rm $DB
+fi
+
+sqlite3 $DB < src/clojure/neveragain/schema.sql
+sqlite3 $DB < test/neveragain/testdata.sql
+
+echo "Done!"
