0dab876f58e9f25665e0d44766dab9f6a6cef03d
Ryan Jenkins <rjenkins@mail.sfsu.edu>
2015-01-24 17:13:22 -0800

    Fixed status message in webmail when view has no messages.

--- src/clojure/neveragain/common.clj
+++ src/clojure/neveragain/common.clj
@@ -302,7 +302,8 @@
           [cipher-text enc-key iv] (encrypt-message message pub-key)]
       ; sqlite's last row id key name doesn't play nice with clojure's syntax
       ; but this'll work
-      (let [message-id (k/insert (k/values 
+      (let [message-id (k/insert e/messages
+                                 (k/values
                                    {:recipient_id (:id user)
                                     :data (->> cipher-text
                                                (b64/encode)
